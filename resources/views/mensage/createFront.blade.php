ee636c8c3f68cfe463147ef91e81bf1fc8f94652
michael101136 <michael101136@gmail.com>
2018-02-07 15:16:26 -0500

    diseño de los layout y creacion  de contacto
    diseño de los layout y creacion  de contacto

--- resources/views/mensage/createFront.blade.php
+++ resources/views/mensage/createFront.blade.php
@@ -1,72 +1,85 @@
 @extends('layoutFront')
 @section('contenido')
-        <div style="">
-			@if(session()->has('info'))
-				<h3> {{ session('info')}} </h3>
+		 <div class="c-content-box c-size-md c-bg-white c-no-bottom-padding">
+		                <div class="container">
+		                    <div class="c-content-product-1 c-opt-1">
+		                        <div class="c-content-title-1">
+		                            <h3 class="c-center c-font-uppercase c-font-bold">CONTACTENOS</h3>
+		                            <div class="c-line-center"></div>
+		                        </div>
+		                        <div class="row">
+		                            <div class="col-md-4 wow animate slideInUp">
+		                                <div class="c-media">
+		                                    <img src="{{URL::asset('/img/contacto/contacto.jpg')}}" alt="" /> </div>
+		                            </div>
+		                            <div class="col-md-8" style="margin-top: -60PX;">
+		                                <div class="c-body">
+		                                    @if(session()->has('info'))
 
-			@endif
-			<div class="panel panel-success"">
-				<div class="panel-heading"> </div>
-				<div class="panel-body">
-					<div class="col-lg-5">
-					</div>
-					<div class="col-lg-5">
-					<form method="POST" class="form-horizontal" action="{{ route('mensajes.store') }}">
-						{!!  csrf_field() !!}
-						
-							
-						<div class="form-group"> 
-							<label  class="col-lg-2 control-label">
-								Nombre
-							</label>
-							<div class="col-lg-5">
-								<input type="text" name="nombre" class="form-control" id="nombre" value="{{old('nombre')}}" placeholder="Nombre">
-								{!! $errors->first('nombre','<span class=error>:message</span>') !!}
-							</div>
-						</div>
+												<h3> {{ session('info')}} </h3>
+		
+											@endif
+													
+												<form method="POST" class="form-horizontal" action="{{ route('mensajes.store') }}">
+														{!!  csrf_field() !!}
+														
+															
+														<div class="form-group"> 
+															<label  class="col-lg-2 control-label">
+																Nombre
+															</label>
+															<div class="col-lg-5">
+																<input type="text" name="nombre" class="form-control" id="nombre" value="{{old('nombre')}}" placeholder="Nombre">
+																{!! $errors->first('nombre','<span class=error>:message</span>') !!}
+															</div>
+														</div>
 
-						<div class="form-group"> 
-							<label  class="col-lg-2 control-label">
-							Email
-							</label>
-							<div class="col-lg-5">
-								<input type="text" class="form-control" name="email" id="email" value ="{{old('nombre')}}" placeholder="Email">
-								{!! $errors->first('email','<span class=error>:message</span>') !!}
-							</div>
-						</div>
-				
+														<div class="form-group"> 
+															<label  class="col-lg-2 control-label">
+															Email
+															</label>
+															<div class="col-lg-5">
+																<input type="text" class="form-control" name="email" id="email" value ="{{old('nombre')}}" placeholder="Email">
+																{!! $errors->first('email','<span class=error>:message</span>') !!}
+															</div>
+														</div>
+												
 
-						<div class="form-group"> 
-							<label  class="col-lg-2 control-label">
-								Mensage
-							</label>
-							<div class="col-lg-5">
-								<input type="text" name="nombre" class="form-control" id="nombre" value="{{ old('nombre') }}" placeholder="Mensage">
-								{!! $errors->first('nombre','<span class=error>:message</span>') !!}
-							</div>
-						</div>
+														<div class="form-group"> 
+															<label  class="col-lg-2 control-label">
+																Mensage
+															</label>
+															<div class="col-lg-5">
+																<input type="text" name="nombre" class="form-control" id="nombre" value="{{ old('nombre') }}" placeholder="Mensage">
+																{!! $errors->first('nombre','<span class=error>:message</span>') !!}
+															</div>
+														</div>
 
-						<div class="form-group"> 
-							<label  class="col-lg-2 control-label">
-								Comentarios
-							</label>
-							<div class="col-lg-5">
+														<div class="form-group"> 
+															<label  class="col-lg-2 control-label">
+																Comentarios
+															</label>
+															<div class="col-lg-5">
 
-								<input type="text" name="comentario" class="form-control" placeholder="Comentarios">
+																<input type="text" name="comentario" class="form-control" placeholder="Comentarios">
 
-							</div>
-						</div>
+															</div>
+														</div>
 
-						<div class="form-group">
-							<div class="col-lg-offset-2 col-lg-10">
-								<input type="submit" value="Enviar" class="btn btn-info"> 
-							</div>
-						</div>
+														<div class="form-group">
+															<div class="col-lg-offset-2 col-lg-10">
+																<input type="submit" value="Enviar" class="btn btn-info"> 
+															</div>
+														</div>
 
-					</form>
-				</div>
-				</div>
-			</div>
+												</form>
 
-	     </div> 
+		                                    
+		                                </div>
+		                            </div>
+		                        </div>
+		                    </div>
+		            </div>
+		    </div>
+     
 @stop
